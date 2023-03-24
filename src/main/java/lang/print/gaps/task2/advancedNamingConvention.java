b2901bb8969594ee3708045e6251ad2469f982a8
edvinasvencevicius <edviz.nzn@gmail.com>
2023-03-24 20:38:30 +0200

    ok

--- src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
+++ src/main/java/lang/print/gaps/task2/advancedNamingConvention.java
@@ -3,14 +3,17 @@ package lang.print.gaps.task2;
 public class advancedNamingConvention {
     private static final int adult_age = 18;
 
-    private int AGE;
+    private int age;
     private int phoneNumber;
+    int Number = 15;
+    int sum = adult_age + Number;
 
-    void CAllToFriend() {
-        //some code
+    void callToFriend() {
+        System.out.println("Calling a friend by number"+phoneNumber);
     }
 
     void callByNumber(int Number) {
-        //some code
+        System.out.println(Number + adult_age + "equals to " + sum);
     }
 }
+
